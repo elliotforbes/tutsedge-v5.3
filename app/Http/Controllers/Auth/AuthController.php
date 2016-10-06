3e76be67cbb5eb2e7897aff9e7c196477e4f6e7b
emforce <emforce@live.com>
2016-10-06 18:41:42 +0100

    Added auth controller back

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Support\Facades\Session;
+use Socialite;
+use Illuminate\Http\Request;
+use Auth;
+use Log;
+use Event;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+    
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+    
+     /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider(Request $request)
+    {
+        return Socialite::driver('github')
+            ->with(['redirect_uri' => env('GITHUB_CALLBACK_URL' ) . '?redirect=' . $request->input('redirect')])
+            ->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback(Request $request)
+    {
+        Log::info("HandleProviderCallback Hit...");
+        $user = Socialite::driver('github')->user();
+        Session::put('user', $user);
+
+        Log::info("Redirecting to redirect");
+        $redirect = $request->input('redirect');
+        
+        if($redirect)
+        {
+            return redirect($redirect);
+        }
+
+        $authUser = $this->findOrCreateUser($user);
+
+        Auth::login($authUser, true);
+                
+        return redirect('/');
+    }
+
+    /**
+     * Return user if exists; create and return if doesn't
+     *
+     * @param $githubUser
+     * @return User
+     */
+    private function findOrCreateUser($githubUser)
+    {   
+        Log::info("Finding or Creating a User");
+        
+        if ($authUser = User::where('github_id', $githubUser->id)->first()) {
+            Log::info("User Found");
+            Auth::login($authUser);
+            Log::info("Logging in existing user");
+            return $authUser;
+        }
+        
+        Log::info("Creating a new user");
+        // Event::fire(new UserCreatedEvent($githubUser));
+        return User::create([
+            'name' => $githubUser->nickname,
+            'email' => $githubUser->email,
+            'github_id' => $githubUser->id,
+            'avatar' => $githubUser->avatar
+        ]);
+    }
+}
