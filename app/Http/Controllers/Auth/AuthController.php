4ee12513caca59428ccbfa10e0b06a512006ae88
emforce <emforce@live.com>
2016-10-08 18:58:51 +0100

    updates to login script

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -17,7 +17,6 @@ class AuthController extends Controller
      */
     public function redirectToProvider()
     {
-        Log::info("HIT");
         $redirectUrl = "https://tutorialedge.net/login";
         return Socialite::driver('github')->redirectUrL($redirectUrl)->redirect();
     }
@@ -31,16 +30,40 @@ class AuthController extends Controller
     {
         $githubUser = Socialite::driver('github')->stateless()->user();
         Log::info($githubUser->id);
-
-        $user = User::where('github_id', $githubUser->id)->first();
         
-        Log::info($user);
+        $authUser = $this->findOrCreateUser($user);
 
-        Auth::login($user);
-
-        Log::info("Successfully Logged In User");
-
+        Auth::login($authUser, true);
+                
         return redirect('/');
 
+    }
+
+
+    /**
+     * Return user if exists; create and return if doesn't
+     *
+     * @param $githubUser
+     * @return User
+     */
+    private function findOrCreateUser($githubUser)
+    {   
+        Log::info("Finding or Creating a User");
+        
+        if ($authUser = User::where('github_id', $githubUser->id)->first()) {
+            Log::info("User Found");
+            Auth::login($authUser);
+            Log::info("Logging in existing user");
+            return $authUser;
+        }
+        
+        Log::info("Creating a new user");
+        // Event::fire(new UserCreatedEvent($githubUser));
+        return User::create([
+            'name' => $githubUser->nickname,
+            'email' => $githubUser->email,
+            'github_id' => $githubUser->id,
+            'avatar' => $githubUser->avatar
+        ]);
     }
 }
