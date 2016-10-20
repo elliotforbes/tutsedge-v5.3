b2dc47970daad6fdb0e7310dc48b600c772d39d2
= <emforce@live.com>
2016-10-20 10:43:34 +0100

    added stuff to dashboard

---
+++ public/app/components/dashboard/dashboard-oauth/dashboard-oauth.controller.js
@@ -0,0 +1,15 @@
+function DashboardOauthController($http, $log) {
+  var ctrl = this;
+
+  ctrl.$onInit = function() {
+    $http.get('oauth/clients')
+      .then(function success(response){
+        $log.log(response);
+      });
+  }
+}
+
+DashboardOauthController.$inject = ['$http', '$log'];
+
+angular.module('dashboard')
+  .controller('DashboardOauthController', DashboardOauthController);
