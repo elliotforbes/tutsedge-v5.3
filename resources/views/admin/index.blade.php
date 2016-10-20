ee6323493fa90ff406c77b14704b57a57c41328b
= <emforce@live.com>
2016-10-20 11:24:43 +0100

    reference

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -22,6 +22,6 @@
     <ng-view></ng-view>
   </div>
 
-  <script src="{{ asset('/js/admin-bundle.js') }}?v=237"></script>
+  <script src="{{ asset('/js/admin-bundle.js') }}?v=238"></script>
 </body>
 </html>
