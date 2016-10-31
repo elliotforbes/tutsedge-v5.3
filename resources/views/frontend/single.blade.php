dddb0f595dcf5a2ab45a6258806be775716f38af
= <emforce@live.com>
2016-10-31 18:22:00 +0000

    added recommended articles at the bottom of the page

--- resources/views/frontend/single.blade.php
+++ resources/views/frontend/single.blade.php
@@ -40,15 +40,38 @@
                     data-ad-format="auto"></ins>
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>  
+                </script> 
 
             </div><!-- .post-body -->
+
+            <div class="recommended-articles">
+                <div class="row">
+                    <h1>Recommended Articles</h1>
+                    @foreach($articles as $article)
+                    <div class="col l4 m4 s12">
+                        <div class="post">
+                            <div class="post-image">
+                                <img src="{{ asset('/uploads/') }}/{{ $article->image_path }}" alt="{{ $article->description }}">
+                            </div>
+                            <div class="post-link">
+                                <a href="{{ url('/') }}/{{ $article->slug }}"><h3>{{ $article->title }}</h3></a>
+                                <p>{{ $article->description }}</p>
+                                <div class="tags">
+                                    @foreach($article->tags as $tag)
+                                    <a href="{{ url('/') }}/tag/{{ $tag->name }}">{{ $tag->name }}</a>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
 
             <div id="comments" class="comment-container">
                 <div class="comments">
                 <div id="disqus_thread"></div>
                 <script>
-
                 /**
                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 
         <link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
         <link rel="stylesheet" href="{{ asset('/css/materialize.min.css', Request::secure() )  }}">
-        <link rel="stylesheet" href="{{ asset('/css/front-end.css' , Request::secure() ) }}?v=36">
+        <link rel="stylesheet" href="{{ asset('/css/front-end.css' , Request::secure() ) }}?v=37">
   
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
         <script src="https://use.fontawesome.com/9b263d09bd.js"></script>
