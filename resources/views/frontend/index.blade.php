61c2be84f3599114bdad5e1213525fa4521d874c
= <emforce@live.com>
2016-11-13 11:45:50 +0000

    updates

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -25,48 +25,52 @@ by teaching you some of the latest and greatest programming frameworks.</h3>
         <h4>Courses</h4>
         <div class="row flexbox-courses">
             
-            <div class="card">
-                <div class="card-image">
-                    <img src="{{ asset('/uploads/angular.jpg') }}">
-                </div>
-                <div class="card-content">
-                    <h5>AngularJS Fundamentals</h5>
-                    <p>Build your very own AngularJS Applications from the ground up and learn how to deliver high quality SPAs</p>
-                </div>
-                <div class="card-action">
-                <a href="{{ url('/course') }}/angular-js-fundamentals">View Now</a>
-                </div>
-            </div>
-        
-        
-        
-            <div class="card">
-                <div class="card-image">
-                    <img src="{{ asset('/uploads/lwjgl3.png') }}">
-                    
-                </div>
-                <div class="card-content">
-                    <h5>LWJGL 3</h5>
-                    <p>Learn the basics of Graphics and Game programming using Java and the highly popular Lightweight Java Game Library 3.</p>
-                </div>
-                <div class="card-action">
-                <a href="{{ url('/course') }}/lwjgl-3-fundamentals">View Now</a>
+            <div class="col l4 m4 s6 course-container">
+                <div class="course-box">
+                    <div class="course-image">
+                        <img src="{{ asset('/uploads/AngularJS-header-image.png') }}">
+                    </div>
+                    <div class="course-content">
+                        <h5>AngularJS Fundamentals</h5>
+                        <p>Build your very own AngularJS Applications from the ground up and learn how to deliver high quality SPAs</p>
+                    </div>
+                    <div class="course-action">
+                    <a href="{{ url('/course') }}/angular-js-fundamentals">View Now</a>
+                    </div>
                 </div>
             </div>
         
         
+            <div class="col l4 m4 s6 course-container">            
+                <div class="course-box">
+                    <div class="course-image">
+                        <img src="{{ asset('/uploads/lwjgl3.png') }}">
+                        
+                    </div>
+                    <div class="course-content">
+                        <h5>LWJGL 3</h5>
+                        <p>Learn the basics of Graphics and Game programming using Java and the highly popular Lightweight Java Game Library 3.</p>
+                    </div>
+                    <div class="course-action">
+                    <a href="{{ url('/course') }}/lwjgl-3-fundamentals">View Now</a>
+                    </div>
+                </div>
+            </div>
         
-            <div class="card">
-                <div class="card-image">
-                    <img src="{{ asset('/uploads/laravel-5.jpg') }}">
-                    
-                </div>
-                <div class="card-content">
-                    <h5>Laravel 5.2 Fundamentals</h5>
-                    <p>In this course we cover the basics for creating a web application using the Laravel 5.2 PHP framework.</p>
-                </div>
-                <div class="card-action">
-                <a href="{{ url('/course') }}/laravel-5">View Now</a>
+        
+            <div class="col l4 m4 s6 course-container">
+                <div class="course-box">
+                    <div class="course-image">
+                        <img src="{{ asset('/uploads/laravel-5.jpg') }}">
+                        
+                    </div>
+                    <div class="course-content">
+                        <h5>Laravel 5.2 Fundamentals</h5>
+                        <p>In this course we cover the basics for creating a web application using the Laravel 5.2 PHP framework.</p>
+                    </div>
+                    <div class="course-action">
+                    <a href="{{ url('/course') }}/laravel-5">View Now</a>
+                    </div>
                 </div>
             </div>
             
