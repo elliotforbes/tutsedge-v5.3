d58d9c15f919b7b948c5fb64b0b3a66d2be2871a
= <emforce@live.com>
2016-11-24 18:31:44 +0000

    changes to all courses page

--- resources/views/frontend/course/all.blade.php
+++ resources/views/frontend/course/all.blade.php
@@ -11,37 +11,65 @@ All Courses
 @section('content')
 <div class="container">
     <div class="course-container row">
-    <h1>All Courses</h1>
-    @foreach($courses as $course)
-        <div class="col s12 m6 l4">
-            <div class="card">
-                <div class="card-image">
-                    <img src="{{ asset('/uploads/artificial-intelligence.jpg') }}">
-                </div>
-                <div class="card-content">
-                    <h5>{{ $course->title }}</h5>
-                    <p>{{ $course->description }}</p>
-                </div>
-                <div class="card-action">
-                <a href="{{ url('/course') }}/{{ $course->slug }}">View Now</a>
+    <h3>All Courses</h3>
+    <div class="row flexbox-courses">
+            
+            <div class="col l4 m4 s12 course-container">
+                <div class="course-box">
+                    <div class="course-image">
+                        <img src="{{ asset('/uploads/AngularJS-header-image.png') }}">
+                    </div>
+                    <div class="course-content">
+                        <h5>AngularJS Fundamentals</h5>
+                        <p>Build your very own AngularJS Applications from the ground up and learn how to deliver high quality SPAs</p>
+                    </div>
+                    <div class="course-action">
+                    <a href="{{ url('/course') }}/angular-js-fundamentals">View Now</a>
+                    </div>
                 </div>
             </div>
+        
+        
+            <div class="col l4 m4 s12 course-container">            
+                <div class="course-box">
+                    <div class="course-image">
+                        <img src="{{ asset('/uploads/lwjgl3.png') }}">
+                        
+                    </div>
+                    <div class="course-content">
+                        <h5>LWJGL 3</h5>
+                        <p>Learn the basics of Graphics and Game programming using Java and the highly popular Lightweight Java Game Library 3.</p>
+                    </div>
+                    <div class="course-action">
+                    <a href="{{ url('/course') }}/lwjgl-3-fundamentals">View Now</a>
+                    </div>
+                </div>
+            </div>
+        
+        
+            <div class="col l4 m4 s12 course-container">
+                <div class="course-box">
+                    <div class="course-image">
+                        <img src="{{ asset('/uploads/laravel-5.png') }}">
+                        
+                    </div>
+                    <div class="course-content">
+                        <h5>Laravel 5.2 Fundamentals</h5>
+                        <p>In this course we cover the basics for creating a web application using the Laravel 5.2 PHP framework.</p>
+                    </div>
+                    <div class="course-action">
+                    <a href="{{ url('/course') }}/laravel-5">View Now</a>
+                    </div>
+                </div>
+            </div>
+            
+            
         </div>
-    @endforeach
     <div class="break"></div>
         
     <div class="pagination">
         <?php echo $courses->render(); ?>
     </div>
-    </div>
-</div>
-
-<div class="gray-container">
-    <div class="container center">
-        <h2>Become a part of TutorialEdge</h2>
-        <p>TutorialEdge.net has been growing for over a year new and we've seen hundreds of thousands of visitors to the site. Join the growing list of developers who have registered for the site and receive free newletters on the latest industry news as well as top articles.</p>
-        <a href="{{ url('/auth/github') }}"><button class="btn btn-default">+ Register Now</button></a>
-        <div class="break"></div>
     </div>
 </div>
 @endsection
