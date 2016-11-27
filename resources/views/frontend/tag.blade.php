a7b5080fe317d77747c28fd1258c5439303bc8bb
= <emforce@live.com>
2016-11-27 18:44:38 +0000

    improved the look of the tag page

--- resources/views/frontend/tag.blade.php
+++ resources/views/frontend/tag.blade.php
@@ -1,31 +1,27 @@
 @extends('layouts.app')
 
-@section('banner')
-<div class="gray-container">
-    <div class="content">
-        <h1>{{ $tag->name }}</h1>
-        <h5 class="center-align">All Articles For Tag: {{ $tag->name }}</h5>
-    </div>
-</div>
-@endsection
-
 @section('content')
-<div class="content">
-    <div class="row">
-        @foreach($articles as $article)
-            <div class="col s12 m12 l12">
-                <div class="card-panel white">
-                <div class="icon valign-wrapper">
-                    <i class="material-icons">description</i>
-                </div>
-                <a href="{{ url('/') }}/{{ $article->slug }}">
-                <h5>{{ $article->title }}</h5>
-                </a>
-                <span><b>{{ $article->author }} @ {{ date("d M, Y",strtotime($article->created_at)) }}</b> : {{ $article->description }}
-                </span>
+<div class="container">
+    <div class="course-container">
+        <h2>{{ $tag->name }}</h2>
+        <div class="row flexbox-courses">
+            @foreach($articles as $lesson)
+                <div class="col l4 m4 s12 course-container">
+                <div class="course-box">
+                    <div class="course-image">
+                        <img src="{{ asset('/uploads/') }}/{{ $lesson->image_path }}" alt="{{ $lesson->description }}">
+                    </div>
+                    <div class="course-content">
+                    <h5 class="course-title">{{ $lesson->title }}</h5>
+                    <p>{{ $lesson->description }}</p>
+                    </div>
+                    <div class="course-action">
+                    <a href="{{url('/') }}/{{ $lesson->slug }}">Read More</a>
+                    </div>
                 </div>
             </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 </div>
 @endsection
