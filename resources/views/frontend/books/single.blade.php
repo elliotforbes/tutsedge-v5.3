9caa25509f28687a5d476d7d181c42e769b2d0f5
= <emforce@live.com>
2016-12-02 23:51:22 +0000

    improved book single page

--- resources/views/frontend/books/single.blade.php
+++ resources/views/frontend/books/single.blade.php
@@ -24,7 +24,7 @@
 
 <div class="content">
     <div class="row">
-        <div class="col s12 m8 l8">
+        <div class="col s12 m9 l9">
             <div class="post-body">
                 {!! $book->body !!}    
                 
@@ -41,32 +41,30 @@
                         
             </div>
         </div><!-- .col -->
-        <div class="sidebar col s12 m4 l4">
+        <div class="sidebar col s12 m3 l3">
+            <div class="left-sidebar-content">
+                <img src="https://avatars0.githubusercontent.com/u/3332224?v=3&s=466" alt="Elliot Forbes Web Developer">
+                <h2>Elliot Forbes</h2>
+                <h4>Software Engineer</h4>
+
+                <p>23 Year old Software Engineer who loves all manner of front end programing.</p>
+                <div class="social-links register">
+                    <h4>Follow Me</h4>
+                    <a target="_blank" href="https://twitter.com/Elliot_F"><button class="register-btn twitter"><i class="fa fa-twitter-square"></i> Twitter</button></a>
+                    <a target="_blank" href="https://www.facebook.com/tutorialedge"><button class="register-btn facebook"><i class="fa fa-facebook-square"></i> Facebook</button></a>             
+                    <a target="_blank" href="https://github.com/emforce"><button class="register-btn github"><i class="fa fa-github-square"></i> Github</button></a>
+                    <a target="_blank" href="https://www.youtube.com/channel/UCwFl9Y49sWChrddQTD9QhRA"><button class="register-btn youtube"><i class="fa fa-youtube-square"></i> Youtube</button></a>
+                </div>
+            </div>
+
             <div class="course-info">
                 <h2>Review Information:</h2>
                 <p><b>Author:</b> Elliot Forbes</p>
                 <p><b>Date Published: </b> {{ date("d M, Y",strtotime($book->created_at)) }}</p>
             </div><!-- .info -->
             
-             <div class="register"><!-- .register -->
-                <h2>Register Now:</h2>
-                
-                <p>Register now for free quickly using your preferred social media platform and receive the latest tutorials and updates straight to your inbox.</p>
-                
-                <div class="row">
-                    <!--<div class="col s12 m6 l6">
-                        <a href="{{ url('/auth/facebook') }}"><button class="register-btn facebook"><i class="fa fa-facebook-square"></i> Facebook</button></a>
-                    </div>
-                    <div class="col s12 m6 l6">
-                        <a href="{{ url('/auth/twitter') }}"><button class="register-btn twitter"><i class="fa fa-twitter-square"></i> Twitter</button></a>
-                        <a href="{{ url('/auth/googleplus') }}"><button class="register-btn google-plus"><i class="fa fa-google-plus-square"></i> Google+</button></a>
-                    </div>-->
-                    <a href="{{ url('/auth/github') }}"><button class="register-btn github"><i class="fa fa-github-square"></i> Github</button></a>
-                </div>
-            </div><!-- .register -->
+             
             
-            <div class="ad"><!-- .ad -->
-                <h2>Sponsor Us:</h2>
                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <!-- article ad -->
                 <ins class="adsbygoogle"
@@ -78,19 +76,9 @@
                 (adsbygoogle = window.adsbygoogle || []).push({});
                 </script>
                 
-                 <p>Want to see your own advertisement here, check out the <a href="{{ url('/contact') }}">contact page</a></p>
-                
-            </div> <!-- .ad -->
+              
         </div>
     </div><!-- .row -->
 </div>
-<div class="comment-container">
-    <div class="comments">
-    @include('frontend.partials._author')
-    </div>
-    <div class="clear"></div>
-</div>
-<!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=emforce"></script>
 
 @endsection
