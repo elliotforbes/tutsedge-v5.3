057449bb04cc75995187fb68f760e8a12df3ff0a
= <emforce@live.com>
2016-12-04 21:12:09 +0000

    didn't work

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
         Log::info("Logging User In...");
         Auth::login($authUser, true);
         Log::info("Redirecting to the homepage");
-        return redirect('/&v=' + date.now());
+        return redirect('/');
 
     }
 
