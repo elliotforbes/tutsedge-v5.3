511d059a47b5bbee8427a6d0f26002747995bdfc
= <emforce@live.com>
2016-12-18 20:41:50 +0000

    removed shitty course constraints

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -21,18 +21,13 @@ class CourseController extends Controller
         $input = Request::all();
         Log::info($input);
         $course = new Course();
-        if(Auth::check())
-        {   
-            if(Auth::user()->github_id == 3332224){
-                $course->title = $input['title'];
-                $course->slug = $input['slug'];
-                $course->body = $input['body'];
-                $course->description = $input['description'];
-                $course->author = 'Elliot Forbes';
-                $course->isLive = '1';
-                $course->save();
-            }
-        }
+        $course->title = $input['title'];
+        $course->slug = $input['slug'];
+        $course->body = $input['body'];
+        $course->description = $input['description'];
+        $course->author = 'Elliot Forbes';
+        $course->isLive = '1';
+        $course->save();
         return response(array(
               'error' => false
           ), 200);
@@ -43,19 +38,14 @@ class CourseController extends Controller
         $input = Request::all();
         Log::info($input);
         $course = Course::whereSlug($slug)->get()->first();
-        if(Auth::check())
-        {   
-            if(Auth::user()->github_id == 3332224){
-                $course->title = $input['title'];
-                $course->slug = $input['slug'];
-                $course->body = $input['body'];
-                $course->description = $input['description'];
-                $course->author = 'Elliot Forbes';
+        $course->title = $input['title'];
+        $course->slug = $input['slug'];
+        $course->body = $input['body'];
+        $course->description = $input['description'];
+        $course->author = 'Elliot Forbes';
 
-                $course->isLive = '1';
-                $course->save();        
-            }
-        }
+        $course->isLive = '1';
+        $course->save();        
         return response(array(
               'error' => false
           ), 200);
