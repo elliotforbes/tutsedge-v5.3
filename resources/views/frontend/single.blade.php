6114ece97e636a09d7ce2c5800611c75a9adb9a5
= <emforce@live.com>
2017-02-22 21:47:13 +0000

    removed ads

--- resources/views/frontend/single.blade.php
+++ resources/views/frontend/single.blade.php
@@ -51,16 +51,7 @@
 
                 {!! Markdown::convertToHtml($lesson->body) !!}     
 
-                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                <!-- article ad -->
-                <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-6782067367590597"
-                    data-ad-slot="5293007688"
-                    data-ad-format="auto"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script> 
+                <!---->
 
             </div><!-- .post-body -->
 
